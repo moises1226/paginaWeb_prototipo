9a77f1d54ede3351d9e0ef403529826d47ba7db5
Moises Aguilar <moises.aguilar.et7@gmail.com>
2024-07-26 17:45:35 -0300

    intercatividad con el panel de menu y opacidad

--- pag-AutosBWM/interacion/index.js
+++ pag-AutosBWM/interacion/index.js
@@ -1,32 +1,20 @@
 let menu = document.getElementById("menu");
 let dropDown = document.querySelector(".dropDown");
-let salir = document.getElementById("salir")
-
-// menu.addEventListener('click', function(){
-//     if(dropDown.style.opacity === "0" || dropDown.style.opacity === ""){
-//         dropDown.style.opacity = "1";
-//         dropDown.style.visibility = "visible";
-//     } else {
-//         dropDown.style.opacity = "0";
-//         dropDown.style.visibility = "hidden";
-//     }
-// });
-
-
-
+let salir = document.getElementById("salir");
+let overlay = document.getElementById("overlay");
+let body = document.body;
 menu.addEventListener('click', function() {
     if (dropDown.style.transform === "translateX(-100%)" || dropDown.style.opacity === "0") {
         dropDown.style.transform = "translateX(0)"; // Vuelve a la posición original
         dropDown.style.opacity = "1"; // Vuelve a ser visible
-    };
+        overlay.style.display = 'block'; // Muestra el overlay
+        dropDown.style.zIndex = '2'
+        body.style.overflow = 'hidden';
+    }
 });
 
-salir.addEventListener('click', function(){
-
-        dropDown.style.transform = "translateX(-100%)"; // Mueve el elemento a la izquierda
-        dropDown.style.opacity = "0"; // Hace que el elemento sea invisible
-    
-
-
-})
-
+salir.addEventListener('click', function() {
+    dropDown.style.transform = "translateX(-100%)"; // Mueve el elemento a la izquierda
+    dropDown.style.opacity = "0"; // Hace que el elemento sea invisible
+    overlay.style.display = 'none'; // Oculta el overlay
+});
